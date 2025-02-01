dc600419eaadf554d8b231c77ac53e12645a8da9
ioansx <ioanstanilais@gmail.com>
2025-02-01 20:33:02 +0100

    remove deprecated code

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,13 +1,3 @@
-import init, { AgreementClient } from "./agreement_client.js";
 import PageMan from "./page_man.js";
 
-await init();
-
-const client = new AgreementClient(window.location.origin);
 const pageMan = new PageMan(client);
-
-window.context = {
-  pages: {
-    man: pageMan,
-  },
-};
