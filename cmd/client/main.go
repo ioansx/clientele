c7732b7443dcbd7f2f1e884181f3d6332d6b9401
ioansx <ioanstanilais@gmail.com>
2025-02-15 08:49:20 +0100

    back to vanilla Go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,20 +3,19 @@
 package main
 
 import (
-// "syscall/js"
-// "github.com/ioansx/clientele/internal/models"
+	"syscall/js"
+	// "github.com/ioansx/clientele/internal/models"
 )
 
 func main() {
-	// wait := make(chan struct{}, 0)
+	wait := make(chan int, 0)
 
 	// js.Global().Set("ManGet", js.FuncOf(ManGet))
 
-	// <-wait
+	<-wait
 }
 
-//export ManGet
-func ManGet() int {
+func ManGet(this js.Value, args []js.Value) any {
 	// dat := models.ManGetOutdto{Output: "hey there"}
 	// return models.Outdto[models.ManGetOutdto]{Dat: dat}
 	return 5
